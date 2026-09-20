8833bdf58498817775bd3d5bc77152c2f7bab61a
irenkipo <i2026k@yahoo.com>
2026-09-20 09:36:06 -0400

    Wire Google Forms subscription field IDs

--- src/assets/js/subscription-config.js
+++ src/assets/js/subscription-config.js
@@ -1,8 +1,8 @@
 window.IREN_KIPO_SUBSCRIPTION = Object.freeze({
   action: "https://docs.google.com/forms/d/e/1FAIpQLSf4CueonKqtg43EaRjTHyjK3V_PbcGvwwzNju_QM2_mjdCspg/formResponse",
   fields: Object.freeze({
-    email: "emailAddress",
-    consent: "",
+    email: "entry.639030799",
+    consent: "entry.2056694574",
     utm_source: "",
     utm_medium: "",
     utm_campaign: "",
